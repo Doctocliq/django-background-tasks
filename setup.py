20cab401f7ddfa700595dd537ed37a0a0e930a4a
walarsh <vimurillo@doctocliq.com>
2023-09-12 23:21:21 +0100

    django4 compatibility

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ version = __import__('background_task').__version__
 classifiers = [c for c in open('classifiers').read().splitlines() if '#' not in c]
 
 setup(
-    name='django-background-tasks',
+    name='django-background-tasks-doctocliq',
     version=version,
     description='Database backed asynchronous task queue',
     long_description=codecs.open('README.rst', encoding='utf-8').read(),
-    author='arteria GmbH, John Montgomery',
-    author_email='admin@arteria.ch',
-    url='http://github.com/arteria/django-background-tasks',
+    author='walarsh',
+    author_email='vimurillo@doctocliq.com',
+    url='https://github.com/Doctocliq/django-background-tasks',
     license='BSD',
     packages=find_packages(exclude=['ez_setup']),
     include_package_data=True,
